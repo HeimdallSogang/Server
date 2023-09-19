dea60cf7b5b972d336fc924ed62757f6afbdf726
Gunhot <lkh0107@sogang.ac.kr>
2023-09-19 15:09:03 +0900

    admin modified(#15)

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Currency)
+
+admin.site.register(Stock)
+
+admin.site.register(Report)
+
+admin.site.register(Analyst)
+
+admin.site.register(Point)
+
+admin.site.register(Writes)
