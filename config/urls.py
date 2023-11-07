ec0c74eec4847bec5e812946c84d60d97420064a
Gunhot <lkh0107@sogang.ac.kr>
2023-11-07 22:49:09 +0900

    deploy update settings.py

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sellpointadministrator/', admin.site.urls),
     path('analysts/', include("analysts.urls")),
     path('reports/', include("reports.urls")),
 ]
