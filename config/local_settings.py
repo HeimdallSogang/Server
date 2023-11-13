c22666fa5f2367829a2a3fc7c465838c3164db1f
MyungJune <kevinshin123@gmail.com>
2023-11-13 19:23:40 +0900

    install django-debug-toolbar

--- config/local_settings.py
+++ config/local_settings.py
@@ -34,9 +34,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "reports",
     "analysts",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware", # django-debug-toolbar requirement
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -118,3 +120,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1", # django-debug-toolbar requirement
+]
