5caae8fd323f63a04faa6795f4090c14d1f290de
softcoffee1 <yunseongho99@gmail.com>
2023-11-15 21:32:12 +0900

    main search API implemented with ORM(no search optimization)

---
+++ search/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from reports.models import Stock, Analyst
+
+
+class StockSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ["id", "name", "code"]
+
+
+class AnalystSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Analyst
+        fields = ["id", "name", "company"]
+
+
+class StockAnlalystSerializers(serializers.Serializer):
+    stocks = StockSearchSerializer(many=True)
+    analysts = AnalystSearchSerializer(many=True)
+
+    def to_representation(self, instance):
+        stocks_queryset = instance["stocks"]
+        analysts_queryset = instance["analysts"]
+
+        stocks_data = StockSearchSerializer(stocks_queryset, many=True).data
+        analysts_data = AnalystSearchSerializer(analysts_queryset, many=True).data
+
+        return {"stocks": stocks_data, "analysts": analysts_data}
