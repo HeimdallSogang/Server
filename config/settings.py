8ba2583dc6c66e14e68456471e2262aabc1c6732
Gunhot <103306105+Gunhot@users.noreply.github.com>
2024-01-17 20:46:02 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ["https://port-0-server-51ih2alrhinkm9.sel5.cloudtype.app/", "'https://*.127.0.0.1'"]
+CSRF_TRUSTED_ORIGINS = ["https://port-0-server-51ih2alrhinkm9.sel5.cloudtype.app/", "https://*.127.0.0.1", "https://*", "http://*"]
 
 
 # Application definition
